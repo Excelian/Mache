00452cf176cb8aaf8e35fb5c4d710e66021e1e39
jbowkett <james@bowkett.info>
2015-07-17 15:04:07 +0000

    Tidyup

--- cassandra-ns/src/test/java/org/mache/CassandraLoaderTest.java
+++ cassandra-ns/src/test/java/org/mache/CassandraLoaderTest.java
@@ -15,12 +15,8 @@ import static org.junit.Assert.assertNotNull;
  * Created on 14/07/2015.
  */
 public class CassandraLoaderTest extends TestCacheLoaderBase {
-
-
-
     @Override
-    protected ExCacheLoader BuildCacheLoader(Class cls) throws Exception {
-
+    protected ExCacheLoader buildCacheLoader(Class cls) throws Exception {
         Cluster cluster = CassandraCacheLoader.connect("10.28.1.140", "BluePrint", 9042);
         return new CassandraCacheLoader<String,TestEntity>(cls, cluster, SchemaOptions.CREATEANDDROPSCHEMA, keySpace);
     }
