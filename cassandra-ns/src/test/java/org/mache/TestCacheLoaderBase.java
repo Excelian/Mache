e4ed41791f1c659347c4df31659b6dc1e8b73b60
jbowkett <james@bowkett.info>
2015-07-17 15:04:32 +0000

    code tidyup

--- cassandra-ns/src/test/java/org/mache/TestCacheLoaderBase.java
+++ cassandra-ns/src/test/java/org/mache/TestCacheLoaderBase.java
@@ -22,11 +22,11 @@ public abstract class TestCacheLoaderBase {
     protected String keySpace = "NoSQL_Nearside_Test_" + new Date().toString();
     private CacheThing<String, TestEntity> cacheThing;
 
-    abstract protected ExCacheLoader BuildCacheLoader(Class cls) throws Exception;
+    abstract protected ExCacheLoader buildCacheLoader(Class cls) throws Exception;
 
     @Before
     public void setUp() throws Exception {
-        cacheThing = new CacheThing<>(BuildCacheLoader(TestEntity.class));
+        cacheThing = new CacheThing<>(buildCacheLoader(TestEntity.class));
     }
 
     @After
@@ -36,7 +36,7 @@ public abstract class TestCacheLoaderBase {
 
     @Test
     public void testCanGetDriverSession() throws Exception {
-        ExCacheLoader cacheloader=BuildCacheLoader(TestEntity.class);
+        ExCacheLoader cacheloader = buildCacheLoader(TestEntity.class);
         CacheThing cache = new CacheThing<>(cacheloader);
         cache.put("test-2", new TestEntity("test-2"));
         cache.get("test-2");
@@ -88,7 +88,7 @@ public abstract class TestCacheLoaderBase {
         cacheThing.put("test-2", new TestEntity("test-2"));
         cacheThing.put("test-3", new TestEntity("test-3"));
         // replace the cache
-        cacheThing = new CacheThing<String, TestEntity>(BuildCacheLoader(TestEntity.class));
+        cacheThing = new CacheThing<String, TestEntity>(buildCacheLoader(TestEntity.class));
 
         TestEntity test = cacheThing.get("test-2");
         assertEquals("test-2", test.pkString);
@@ -154,7 +154,7 @@ public abstract class TestCacheLoaderBase {
     public void testPutComposite() throws Exception {
 
         CacheThing<CompositeKey, TestEntityWithCompositeKey> compCache = new CacheThing<CompositeKey, TestEntityWithCompositeKey>(
-                BuildCacheLoader(TestEntityWithCompositeKey.class) );
+                buildCacheLoader(TestEntityWithCompositeKey.class) );
 
         TestEntityWithCompositeKey value = new TestEntityWithCompositeKey("neil", "mac", "explorer");
         compCache.put(value.compositeKey, value);
