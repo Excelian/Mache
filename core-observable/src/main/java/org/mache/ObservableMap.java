7e0261880fd1d0a0f08216f62451c4df8b8de426
jbowkett <james_bowkett@lyagushka.co.uk>
2015-07-17 14:56:27 +0000

    Corrected generics

--- core-observable/src/main/java/org/mache/ObservableMap.java
+++ core-observable/src/main/java/org/mache/ObservableMap.java
@@ -27,7 +27,7 @@ public class ObservableMap<K,V> implements ExCache<K,V> {
     }
 
     @Override
-    public Object get(K k) throws ExecutionException {
+    public V get(K k) throws ExecutionException {
         return delegate.get(k);
     }
 
