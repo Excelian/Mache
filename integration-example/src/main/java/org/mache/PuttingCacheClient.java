bc472b140106e74eab493191e99c17fc82b20905
jbowkett <james@bowkett.info>
2015-07-21 11:35:54 +0000

    Made the putting example able to use either cassandra or Mongo

--- integration-example/src/main/java/org/mache/PuttingCacheClient.java
+++ integration-example/src/main/java/org/mache/PuttingCacheClient.java
@@ -1,34 +1,87 @@
 package org.mache;
 
+import org.mache.examples.cassandra.CassandraAnnotatedMessage;
+import org.mache.examples.cassandra.CassandraExample;
 import org.mache.examples.mongo.MongoAnnotatedMessage;
 import org.mache.examples.mongo.MongoExample;
 
 import javax.jms.JMSException;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by jbowkett on 17/07/15.
  */
 public class PuttingCacheClient {
+  private enum CacheType {Cassandra, Mongo}
 
-  public static void main(String...args)  {
-    final int count = extractCountFrom(args);
-    try(final MongoExample mongoEg = new MongoExample()) {
-      final ExCache<String, MongoAnnotatedMessage> cache = mongoEg.exampleCache();
-      //    final ExCache<String, Message> cache = new CassandraExample().exampleCache();
-      System.out.println("Putting....");
-      for (int i = 0; i < count; i++) {
-        final MongoAnnotatedMessage v = new MongoAnnotatedMessage("msg_" + i, "Hello World - " + i);
-        cache.put(v.getPrimaryKey(), v);
-      }
-      System.out.println("Put complete.");
+  public static void main(String...commandLine) {
+    final Args args = parseArgs(commandLine);
+    final int count = args.count;
+    switch(args.cacheType){
+      case Cassandra:
+        doCassandra(count);
+        break;
+      case Mongo:
+        doMongo(count);
+        break;
     }
-    catch (JMSException | IOException e) {
+  }
+
+  private static void doMongo(int count) {
+    try (final MongoExample mongoExample = new MongoExample()) {
+      doMongoExample(count, mongoExample);
+    }
+    catch (IOException | JMSException | ExecutionException e) {
       e.printStackTrace();
     }
   }
 
-  private static int extractCountFrom(String[] args) {
-    return args.length == 1 ? Integer.parseInt(args[0]) : 100;
+  private static void doCassandra(int count) {
+    try (final CassandraExample mongoExample = new CassandraExample()) {
+      doCassandraExample(count, mongoExample);
+    }
+    catch (JMSException | IOException | ExecutionException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void doMongoExample(int count, MongoExample mongoExample) throws IOException, JMSException, ExecutionException {
+    final ExCache<String, MongoAnnotatedMessage> cache = mongoExample.exampleCache();
+    System.out.println("Putting...");
+    for (int i = 0; i < count ; i++) {
+      final MongoAnnotatedMessage v = new MongoAnnotatedMessage("msg_" + i, "Hello World - " + i);
+      cache.put(v.getPrimaryKey(), v);
+    }
+  }
+
+  private static void doCassandraExample(int count, CassandraExample example) throws IOException, JMSException, ExecutionException {
+    final ExCache<String, CassandraAnnotatedMessage> cache = example.exampleCache();
+    System.out.println("Putting...");
+    for (int i = 0; i < count ; i++) {
+      final CassandraAnnotatedMessage v = new CassandraAnnotatedMessage("msg_" + i, "Hello World - " + i);
+      cache.put(v.getPrimaryKey(), v);
+    }
+  }
+
+  private static Args parseArgs(String[] args) {
+    if (args.length == 2){
+      final CacheType cacheType = CacheType.valueOf(args[1]);
+      final int count = Integer.parseInt(args[0]);
+      return new Args(count, cacheType);
+    }
+    else{
+      throw new RuntimeException("Usage : PuttingCacheClient <put count> "+ Arrays.toString(CacheType.values()));
+    }
+  }
+
+  private static class Args {
+    private final int count;
+    private final CacheType cacheType;
+    public Args(int count, CacheType cacheType) {
+      this.count = count;
+      this.cacheType = cacheType;
+    }
   }
 }
