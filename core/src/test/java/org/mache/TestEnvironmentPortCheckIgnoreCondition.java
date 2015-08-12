7675340b41d3141fa0b075308888ec66636089bd
the-voice <other@outthere.com>
2015-08-12 14:48:14 +0100

    renamed variable as no longer static

--- core/src/test/java/org/mache/TestEnvironmentPortCheckIgnoreCondition.java
+++ core/src/test/java/org/mache/TestEnvironmentPortCheckIgnoreCondition.java
@@ -9,22 +9,22 @@ public abstract class TestEnvironmentPortCheckIgnoreCondition implements Conditi
         if(new NotRunningInExcelian().isSatisfied()) {
             if (PortCheck.PortAccessible("localhost", port)) {
                 hostName = "localhost";
-                FOUND_IT = true;
+                hostPresent = true;
             }
         }
         else {
             hostName = "10.28.1.140";// internal test environment - we don't check port as we'd like to fail if service is down
-            FOUND_IT = true;
+            hostPresent = true;
         }
     }
 
     private static String hostName="not-set";
-    private boolean FOUND_IT=false;
+    private boolean hostPresent =false;
 
     public static String HostName(){
         return hostName;
     }
     public boolean isSatisfied(){
-        return !FOUND_IT;
+        return !hostPresent;
     }
 }
