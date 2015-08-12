bfd20e72ed83a3aa1077ace5906e48c79c4bdd18
the-voice <other@outthere.com>
2015-08-12 16:53:46 +0100

    turning off kafka tests again. now I have better logging

--- core-observable/src/test/java/org/mache/integrations/eventing/KafkaEventingTest.java
+++ core-observable/src/test/java/org/mache/integrations/eventing/KafkaEventingTest.java
@@ -3,6 +3,7 @@ package org.mache.integrations.eventing;
 import com.codeaffine.test.ConditionalIgnoreRule;
 import org.junit.Rule;
 import org.mache.NoRunningKafkaForTests;
+import org.mache.NotRunningInExcelian;
 import org.mache.events.MQFactory;
 import org.mache.events.integration.KafkaMQFactory;
 
@@ -11,7 +12,7 @@ import java.io.IOException;
 
 import static com.codeaffine.test.ConditionalIgnoreRule.*;
 
-@IgnoreIf(condition = NoRunningKafkaForTests.class)
+@IgnoreIf(condition = NotRunningInExcelian.class)
 public class KafkaEventingTest extends TestEventingBase {
 
     @Rule
