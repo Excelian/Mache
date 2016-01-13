1c4375b6f042224a6e7089010a63920fa426f964
Darren <the-voice@users.noreply.github.com>
2016-01-13 13:11:45 +0000

    Update CouchbaseCacheLoader.java
    removed static for cluster

--- mache-couchbase/src/main/java/com/excelian/mache/couchbase/CouchbaseCacheLoader.java
+++ mache-couchbase/src/main/java/com/excelian/mache/couchbase/CouchbaseCacheLoader.java
@@ -29,7 +29,7 @@ public class CouchbaseCacheLoader<K, V> extends AbstractCacheLoader<K, V, Cluste
 
     private Class<K> keyType;
     private Class<V> valueType;
-    private static Cluster cluster;
+    private final Cluster cluster;
     private ClusterManager manager;
     private CouchbaseTemplate template;
     private BucketSettings bucketSettings;
