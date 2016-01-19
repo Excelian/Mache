d79a1135f58163a7cfe217accd534ff9d91c113f
the-voice <other@outthere.com>
2016-01-19 10:53:14 +0000

    see if we can compile c# in travis

---
+++ mono-example/MonoExampleTests/ExampleTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MonoExampleTests
+{
+    [TestFixture]
+    public class ExampleTest
+    {
+        [Test]
+        public void MyTest()
+        {
+            int a = 10;
+            Assert.AreEqual(10, a);
+        }
+    }
+}
