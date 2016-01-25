cac8fcb3a83fd1286cf09306cc48da74372b1769
Jamie Drummond <jamie.drummond@excelian.com>
2016-01-25 10:21:07 +0000

    run tests after gradle check run in background even without tty (no idea how travis is setup) some extra logging

--- config/travis-scripts/run-rest-test.sh
+++ config/travis-scripts/run-rest-test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 gradle :mache-vertx:installApp
-./mache-vertx/build/install/mache-vertx/bin/mache-vertx &
-nc -z -v -w5 localhost 8080 || { echo 'unable to start vertx on 8080' ; exit 1; }
+nohup ./mache-vertx/build/install/mache-vertx/bin/mache-vertx &
+MACHE-PID=$!
+echo 'Mache started, PID $MACHE-PID'
+nc -z -v -w 10 localhost 8080 || { echo 'unable to start vertx on 8080' ; exit 1; }
 
 python mache-example/src/examples/python/MacheRestTest.py || { echo 'Python REST test failed' ; exit 1; }
 
+kill $MACHE-PID
