67fc00ec60b7920130f334e7bf8d2ce885699757
Jamie Drummond <jamie.drummond@excelian.com>
2016-01-25 10:02:05 +0000

    JavaScript/Node REST example requests

---
+++ mache-example/src/examples/javascript/MacheRestTest.js
@@ -0,0 +1,46 @@
+var http = require('http')
+var assert = require('assert');
+
+var base_url = "http://localhost:8080/map"
+
+getShouldFailUnknownKey = function () {
+    http.request(base_url + "/unknown/2", function(res) {
+        assert.equal(400, res.statusCode)
+    }).end();
+}();
+
+putAndGetShouldReturnSameValue = function () {
+    var requestData = { 'make': 'honda', 'model': 'accord' }
+
+    var putRequest = http.request({
+        hostname: 'localhost',
+        port: 8080,
+        path: '/map/cars/1',
+        method: "PUT",
+        headers: {
+            "content-type": "application/json",
+        }
+    }, function(res) {
+        // Once put complete send the get
+        assert.equal(200, res.statusCode)
+
+        http.request({
+            hostname: 'localhost',
+            port: 8080,
+            path: '/map/cars/1'
+        }, function(resGet) {
+            assert.equal(200, resGet.statusCode)
+
+            var body = '';
+            resGet.on('data', function (chunk) {
+                body += chunk;
+            });
+            resGet.on('end', () => {
+                var result = JSON.parse(body)
+                assert.equal('honda', result.make)
+            });
+        }).end();
+    })
+    putRequest.write(JSON.stringify(requestData))
+    putRequest.end();
+}();
