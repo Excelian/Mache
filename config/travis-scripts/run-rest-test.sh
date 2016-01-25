5478bdee7f545df59360382e717ce45d9e33d988
Jamie Drummond <jamie.drummond@excelian.com>
2016-01-25 10:09:11 +0000

    Run vertx in background.. oops

--- config/travis-scripts/run-rest-test.sh
+++ config/travis-scripts/run-rest-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gradle :mache-vertx:installApp
-./mache-vertx/build/install/mache-vertx/bin/mache-vertx
+./mache-vertx/build/install/mache-vertx/bin/mache-vertx &
 nc -z -v -w5 localhost 8080 || { echo 'unable to start vertx on 8080' ; exit 1; }
 
 python mache-example/src/examples/python/MacheRestTest.py || { echo 'Python REST test failed' ; exit 1; }
