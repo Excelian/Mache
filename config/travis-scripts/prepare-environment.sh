16ad0f9d28a47027d67634b51bb499ab0f5d562c
Jamie Drummond <jamie.drummond@excelian.com>
2016-01-25 13:04:30 +0000

    npm install for node to work

--- config/travis-scripts/prepare-environment.sh
+++ config/travis-scripts/prepare-environment.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-sudo npm cache clean -f
-sudo npm install -g n
-sudo n 4.2.2
 
 sudo pip install requests
 
 sudo apt-get -qq update
-sudo apt-get -qq install -y mono-devel mono-gmcs nunit-console
+sudo apt-get -qq install -y mono-devel mono-gmcs nunit-console npm
+
+# Node, requires npm
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n 4.2.2
