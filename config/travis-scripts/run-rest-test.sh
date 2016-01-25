7c3a8f4f4a9df9c7ebd63802e3432716a49ba7b6
Jamie Drummond <jamie.drummond@excelian.com>
2016-01-25 10:47:02 +0000

    netcat try connect loop

--- config/travis-scripts/run-rest-test.sh
+++ config/travis-scripts/run-rest-test.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 gradle :mache-vertx:installApp
-nohup ./mache-vertx/build/install/mache-vertx/bin/mache-vertx &
+./mache-vertx/build/install/mache-vertx/bin/mache-vertx &
 MACHE-PID=$!
-echo 'Mache started, PID $MACHE-PID'
-nc -z -v -w 10 localhost 8080 || { echo 'unable to start vertx on 8080' ; exit 1; }
+
+NEXT_WAIT_TIME=0
+until nc -z -v -w 1 localhost 8080 || [ $NEXT_WAIT_TIME -eq 5 ]; do
+   sleep $(( NEXT_WAIT_TIME++ ))
+done
+
+if [ $NEXT_WAIT_TIME -eq 5 ] && [ $? -ne 0 ]; then
+   echo 'unable to start vertx on 8080'
+   exit 1
+fi
+
+echo "Mache started, PID $MACHE-PID"
 
 python mache-example/src/examples/python/MacheRestTest.py || { echo 'Python REST test failed' ; exit 1; }
 
