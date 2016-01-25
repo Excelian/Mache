42916b938db22a4655107bc7f65fd1ed687fd3cf
Jamie Drummond <jamie.drummond@excelian.com>
2016-01-25 13:17:58 +0000

    Try retaining environment when calling n (Travis sudo command shows n as missing)
    Some logging for debugging environment setup

--- config/travis-scripts/prepare-environment.sh
+++ config/travis-scripts/prepare-environment.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+echo "Installing requests for python"
 sudo pip install requests
 
+echo "apt-get for mono/nunit/npm packages"
 sudo apt-get -qq update
 sudo apt-get -qq install -y mono-devel mono-gmcs nunit-console npm
 
-# Node, requires npm
+echo "Installing n for node"
 sudo npm cache clean -f
 sudo npm install -g n
-sudo n 4.2.2
+
+echo "Installing node version 4.2.2"
+sudo -E n 4.2.2
